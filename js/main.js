6206d336d6535fc1642308bab5ef905343a60152
zuczekann <127730541+zuczekann@users.noreply.github.com>
2023-03-22 11:13:24 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+let firstNumber = 7;
+let secondNumber = 31;
+
+let a 
+let b 
+function myFunction(a, b) {
+    return a + b;
+}
+
+let x
+x = myFunction(firstNumber, secondNumber);
+console.log(x)
+
+
+
+
+
